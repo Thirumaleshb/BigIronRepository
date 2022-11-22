bead804b7be071d3cbdfb6d5a15ed239d55a61a7
Thirumaleshb <63893075+Thirumaleshb@users.noreply.github.com>
2022-11-22 17:09:16 +0530

    Adding Home Page of UI

---
+++ ui/src/components/Home.svelte
@@ -0,0 +1,97 @@
+<div class="container">
+    <div id="carouselExampleIndicators" class="carousel slide"  data-interval="false">
+            <div class="carousel-inner ps-1">
+                <div class="carousel-item active "> 
+                <a href="#"><img class="img-fluid" src="https://bigiron.blob.core.windows.net/public/slider/realestate-banner-ra.jpg" width="854" height="214" alt="First  Slide Image" ></a>
+                </div>
+                <div class="carousel-item">
+                <a href="#"><img class="img-fluid" src="https://bigiron.blob.core.windows.net/public/slider/coloradoffaslider2webready-ra.jpg" width="854" height="214" alt="Second Slide Image" ></a>
+                </div>
+                <div class="carousel-item">
+                <a href="#"><img class="img-fluid" src="https://bigiron.blob.core.windows.net/public/slider/hitchpin-banner-ra.jpg" width="854" height="214" alt="Third Slide Image" ></a>
+                </div>
+                <div class="carousel-item">
+                    <a href="#"><img class="img-fluid" src="https://bigiron.blob.core.windows.net/public/slider/sunstateauctions-banner2-ra.jpg" width="854" height="214" alt="Fourth Slide Image" ></a>
+                </div>
+                <div class="carousel-item">
+                    <a href="#"><img class="img-fluid" src="https://bigiron.blob.core.windows.net/public/slider/mahindra-banner2-ra.jpg" width="854"  height="214"
+                        alt="Fifth Slide Image" ></a>
+                </div>
+            </div>
+
+            <div class="carousel-indicators mx-0 my-3 text-center">
+                <button class="bg-dark active" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" aria-current="true" aria-label="Slide 1"></button>
+                <button class="bg-dark" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button class="bg-dark" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                <button class="bg-dark" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                <button class="bg-dark" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+            </div>
+
+        </div>
+<div id="auctionItems" class="mt-5 border border-2 border-black">
+    <div class="py-4 px-3" >
+    <div class="Heading-text bg-black ms-1">
+        <a class="text-decoration-none text-primary text-center" href="#" ><h1 class="py-1 fw-bold mb-1" style="font-size:22px">-Highlighted Auctions-</h1></a>
+    </div>
+    <div class="row mt-1 mx-0">
+        <div class="col-lg-3 col-sm-4 col-6 mb-2 px-1">
+                <div class="auctionImage mx-auto">
+                <a class="text-decoration-none text-center" href="#">
+                    <div ><img  class="img-fluid" src="https://bigiron.blob.core.windows.net/thumbnail/consignments/0551827fd44bed119848000d3a3c6e8c/sterlingdalehutchinsretirement-4.jpg" width="250" height="150"></div>
+                    <div class="fw-bold text-nowrap fs-8">
+                        <p class="text-darkblue mb-0 text-truncate">Dale & Illene Hutchins Retirement</p>
+                        <p class="mb-0 text-lightDark text-truncate">Geuda Springs,Ks</p>
+                        <p class="mb-0 text-lightDark text-truncate">Nov 30,2022-Equipment Auction</p>
+                    </div>
+                </a>
+                </div>
+            
+        </div>
+
+        <div class="col-lg-3 col-sm-4 col-6 mb-2 px-1">
+                <div class="auctionImage mx-auto">
+                <a class="text-decoration-none text-center" href="#">
+                    <div><img class="img-fluid" src="https://bigiron.blob.core.windows.net/thumbnail/items/8966478e5d3bed1197b00003fff934d4/8530mfwdtractor_b64a4c5ba5e54b61a8d198e769e203d8.jpg" width="250" height="150"></div>
+                    <div class="fw-bold text-nowrap fs-8">
+                        <p class="text-darkblue mb-0 text-truncate">Lau Farm Corp Retirement</p>
+                        <p class="mb-0 text-lightDark  text-truncate">Denver,IA</p>
+                        <p class="mb-0 text-lightDark text-truncate">Nov 30,2022-Equipment Auction</p>
+                    </div>
+                </a>
+                </div>
+            
+        </div>
+
+        <div class="col-lg-3 col-sm-4 col-6 mb-2 px-1 ">
+                <div class="auctionImage mx-auto">
+                <a class="text-decoration-none  text-center" href="#">
+                    <div ><img class="img-fluid" src="https://bigiron.blob.core.windows.net/thumbnail/items/8966478e5d3bed1197b00003fff934d4/8530mfwdtractor_b64a4c5ba5e54b61a8d198e769e203d8.jpg" width="250" height="150"></div>
+                    <div class="fw-bold text-nowrap fs-8">
+                        <p class="text-darkblue mb-0 text-truncate">Lau Farm Corp Retirement</p>
+                        <p class="mb-0 text-lightDark text-truncate">Denver,IA</p>
+                        <p class="mb-0 text-lightDark  text-truncate">Nov 30,2022-Equipment Auction</p>
+                    </div>
+                </a>
+                </div>
+            
+        </div>
+        <div class="col-lg-3 col-sm-4 col-6 mb-2 px-1">
+                <div class="auctionImage mx-auto ">
+                <a class="text-decoration-none text-center" href="#">
+                    <div><img class="img-fluid" src="https://bigiron.blob.core.windows.net/thumbnail/items/8966478e5d3bed1197b00003fff934d4/8530mfwdtractor_b64a4c5ba5e54b61a8d198e769e203d8.jpg" width="250" height="150"></div>
+                    <div class="fw-bold text-nowrap fs-8">
+                        <p class="text-darkblue mb-0 text-truncate">Lau Farm Corp Retirement</p>
+                        <p class="mb-0 text-lightDark  text-truncate">Denver,IA</p>
+                        <p class="mb-0 text-lightDark text-truncate">Nov 30,2022-Equipment Auction</p>
+                    </div>
+                </a>
+                </div>
+            
+        </div>
+
+        
+    </div>
+    </div>
+</div>
+</div>
+  
